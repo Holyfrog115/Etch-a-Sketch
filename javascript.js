060cb4d4db44e61f7c89e0593cba4abfd47358f5
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-05-30 10:49:39 +0700

    Add function for deleting grid box

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-function addBox() {
+function addBox(sideNumber) {
     const container = document.querySelector(".container");
     const box = document.createElement("div");
     box.classList.add("box");
@@ -12,20 +12,29 @@ function addBox() {
 
 function createGridBox(sideNumber) {
     // Creates a grid of 16 boxes
-
     for (let i = 0; i < sideNumber * sideNumber; i++) {
-        addBox();
+        addBox(sideNumber);
     }
+}
+
+
+function deleteGridBox() {
+    const container = document.querySelector(".container");
+    container.replaceChildren();
 }
 
 
 function newGridButton() {
     const btn = document.querySelector('#newGrid');
-    let squaresNumber = 0;
     btn.addEventListener("click", (event) => {
+        let squaresNumber = 0;
         squaresNumber = +prompt("Number of sqares per side?");
         if (squaresNumber < 1 || squaresNumber > 100) {
             alert("You can choose from 1 up to 100 squares per side");
+        }
+        else {
+            deleteGridBox()
+            createGridBox(squaresNumber);
         }
     });
 }
