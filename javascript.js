d8dd63c6bec45a622ade6c7ee2d42cccc6309b43
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-05-30 10:38:51 +0700

    Add prompt for number on 1 side on New Grid button
    Also add parameter sideNumber to createGridBox, so it can create
    grids of different size.

--- javascript.js
+++ javascript.js
@@ -10,17 +10,30 @@ function addBox() {
 }
 
 
-function createGridBox() {
+function createGridBox(sideNumber) {
     // Creates a grid of 16 boxes
 
-    for (let i = 0; i < 16 * 16; i++) {
+    for (let i = 0; i < sideNumber * sideNumber; i++) {
         addBox();
     }
 }
 
 
+function newGridButton() {
+    const btn = document.querySelector('#newGrid');
+    let squaresNumber = 0;
+    btn.addEventListener("click", (event) => {
+        squaresNumber = +prompt("Number of sqares per side?");
+        if (squaresNumber < 1 || squaresNumber > 100) {
+            alert("You can choose from 1 up to 100 squares per side");
+        }
+    });
+}
+
+
 function main() {
-    createGridBox();
+    createGridBox(16);
+    newGridButton();
 }
 
 
